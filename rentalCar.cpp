6aa3f4c636d599e87e030bd018dfa4bd8f69cb9f
hamza10vr <ha.mza@hotmail.com>
2021-09-07 14:03:41 -0500

    only 1 requirement left

--- rentalCar.cpp
+++ rentalCar.cpp
@@ -8,14 +8,14 @@
 #include <fstream>
 #include <vector>
 #include <iomanip>
-using namespace std;
+
 
 
 class rentalCar
 {
 public:
 	rentalCar();									// default constructor
-	rentalCar(int id, std::string make, std::string model) { this->id = id; this->make; make; this->model = model; }	// parameterized constructor
+	rentalCar(int id, std::string make, std::string model) { this->id = id; this->make= make; this->model = model; }	// parameterized constructor
 
 	/************ Setters    *****************************/
 	void setid(int id) { this->id = id; }
@@ -23,22 +23,26 @@ public:
 	void setModel(std::string model) { this->model = model; }
 
 	/************ Getters    **************************/
-	int getid() { return this->id; }
+	int getid() const { return this->id; }
 	std::string getMake() const { return this->make; }
 	std::string getModel() const { return this->model; }
 
 	//**********  Helper functions  ****//
-	void searchId(vector<rentalCar>& cars);		// searches id against car database and prints results to screen
-	void searchMake(vector<rentalCar>& cars);	// searches Make against car database and prints results to screen
-	void searchModle(vector<rentalCar>& cars);	// searches MODLE against car database and prints results to screen
+	void searchId(std::vector<rentalCar>& cars) const;		// searches id against car database and prints results to screen
+	void searchMake(std::vector<rentalCar>& cars) const;	// searches Make against car database and prints results to screen
+	void searchModle(std::vector<rentalCar>& cars) const ;	// searches MODLE against car database and prints results to screen
+	void addCar(std::vector<rentalCar>& cars);		// add a new car (new car id cannot be same as existing ones)
+	void delCarbyID(std::vector<rentalCar>& cars);	// delete an existing car by id
+
+	void ReadandStore(std::vector<rentalCar>& classobj); // reads the data from cars.dat file and stores it in vector
+	void listAllCars(std::vector<rentalCar>& cars);
 
 
 
-	void apptoCarsData(fstream& out);
+	//void apptoCarsData(fstream& out);
 
-	void listAllCars(std::vector <rentalCar>& cars);
 
-	void ReadandStore(std::vector<rentalCar>& classobj);
+
 
 
 private:
@@ -88,11 +92,11 @@ int main()
 			break;
 
 		case 4:
-			cout << "That's 32 human years." << endl;
+			cars.at(0).addCar(cars);
 			break;
 
 		case 5:
-			cout << "That's 37 human years." << endl;
+			cars.at(0).delCarbyID(cars);
 			break;
 
 		case 6:
@@ -100,13 +104,13 @@ int main()
 			break;
 
 		case 7:
-			cout << "\n\n******** See you soon ********\n" << endl;
+			std::cout << "\n\n******** See you soon ********\n" << std::endl;
 	
 			break;
 		default:
 		{
-			cout << "\n******** not a valid option ********" << endl;
-			cout << "******** Please choose a number from 1 to 7  ********\n" << endl;
+			std::cout << "\n******** not a valid option ********" << std::endl;
+			std::cout << "******** Please choose a number from 1 to 7  ********\n" << std::endl;
 		}
 			break;
 		}
@@ -128,134 +132,10 @@ rentalCar::rentalCar()
 	
 }
 
-void rentalCar::listAllCars(std::vector<rentalCar>& cars)
-{
-	std::cout << "\n\n" << setw(6) << left << "ID" << "|" << setw(10) << left << "  Make" << "|" << setw(10) << left << "  Model" << std::endl;
-	std::cout << setfill('-') << setw(26) << "" << std::endl;
-	std::cout << setfill(' ');
-
-	for (unsigned int i = 0; i < cars.size(); ++i)
-	{
-		std::cout << setw(6) << left << cars.at(i).getid() << "|" << setw(10) << left << cars.at(i).getMake() << "|" << setw(10) << left << cars.at(i).getModel() << std::endl;
-	}
-	std::cout << std::endl;
-}
-// searches id against car database and prints results to screen
-void rentalCar::searchId(vector<rentalCar>& cars)
-{
-	{
-		int searchID;
-
-		std::cout << "you can choose from the following ' MODEL ' :\n";
-		for (int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different ID available
-			std::cout << setw(5) << left << cars.at(i).getid() << " | ";
-		std::cout << "\nSearch by ID (first letter is capital for all makes): ";
-		std::cin >> searchID;
-
-		int count = 0;
-		for (int i = 0; i < cars.size(); ++i)		// lopp to iterate through the database searching for id
-		{
-			if (cars.at(i).getid() == searchID)		//if id found, increases cout and prints out the results
-			{
-				count++;
-				std::cout << "\n\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
-				std::cout << setw(6) << left << "ID" << "|" << setw(10) << left << "  Make" << "|" << setw(10) << left << "  Model" << std::endl;
-				std::cout << setfill('-') << setw(26) << "" << std::endl;
-				std::cout << setfill(' ');
-				std::cout << setw(6) << left << cars.at(i).getid() << "|" << setw(10) << left << cars.at(i).getMake() << "|" << setw(10) << left << cars.at(i).getModel() << std::endl ;
-				break;
-			}			
-		}
-		if (count == 0)		// if count not increased it means id not found and prints respective message to the screen
-		{
-			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
-			std::cout << "Please try again" << std::endl;
-			std::cout << "****************************************************** \n\n" << std::endl;
-			
-		}
-
-	}
-}
-// searches id against car database and prints results to screen
-void rentalCar::searchMake(vector<rentalCar>& cars)
-{
-	{
-		string searchMake;
-
-		std::cout << "you can choose from the following ' Make ' :\n";
-		for (int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different makes available
-			std::cout << setw(5) << left << cars.at(i).getMake() << " | ";
-		std::cout << "\nSearch by Make (first letter is capital for all makes): ";
-		std::cin >> searchMake;
-
-		std::cout << setw(6) << left << "ID" << "|" << setw(10) << left << "  Make" << "|" << setw(10) << left << "  Model" << std::endl;
-		std::cout << setfill('-') << setw(26) << "" << std::endl;
-		std::cout << setfill(' ');
-		int count = 0;
-		for (int i = 0; i < cars.size(); ++i)			// lopp to iterate through the database searching for "MAKE"
-		{
-			if (cars.at(i).getMake() == searchMake)		//if " MAKE " found, increases cout and prints out the results
-			{
-				count++;
-				std::cout << setw(6) << left << cars.at(i).getid() << "|" << setw(10) << left << cars.at(i).getMake() << "|" << setw(10) << left << cars.at(i).getModel() << std::endl ;
-				
-			}
-		}
-		if (count <= 0)			// if count not increased it means "MAKE " not found and prints respective message to the screen
-		{
-			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
-			std::cout << "Please try again" << std::endl;
-			std::cout << "****************************************************** \n\n" << std::endl;
-		}
-		
-	}
-}
-
-void rentalCar::searchModle(vector<rentalCar>& cars)
-{
-	{
-		string searchModel;
-
-		std::cout << "you can choose from the following ' MODEL ' :\n";
-		for (int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different MODEL available
-			std::cout << setw(5) << left << cars.at(i).getModel() << " | ";
-		std::cout << "\nSearch by MODEL (first letter is capital for all makes): ";
-		std::cin >> searchModel;
-
-		std::cout << setw(6) << left << "ID" << "|" << setw(10) << left << "  Make" << "|" << setw(10) << left << "  Model" << std::endl;
-		std::cout << setfill('-') << setw(26) << "" << std::endl;
-		std::cout << setfill(' ');
-		int count = 0;
-		for (int i = 0; i < cars.size(); ++i)			// lopp to iterate through the database searching for "MODEL"
-		{
-			if (cars.at(i).getModel() == searchModel)		//if " MODEL " found, increases cout and prints out the results
-			{
-				count++;
-				std::cout << setw(6) << left << cars.at(i).getid() << "|" << setw(10) << left << cars.at(i).getMake() << "|" << setw(10) << left << cars.at(i).getModel() << std::endl;
-
-			}
-		}
-		if (count <= 0)			// if count not increased it means "MODLE " not found and prints respective message to the screen
-		{
-			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
-			std::cout << "Please try again" << std::endl;
-			std::cout << "****************************************************** \n\n" << std::endl;
-		}
-
-	}
-}
-
-void rentalCar::apptoCarsData(fstream& out)
-{
-	std::cout << id << std::endl;
-	std::cout << make << std::endl;
-	std::cout << model << std::endl;
-}
-
 void rentalCar::ReadandStore(std::vector<rentalCar>& classobj)
 {
-	ifstream inFS;
-	
+	std::ifstream inFS;
+
 
 	//trying to open file
 	std::cout << "opening file cars.data" << std::endl;
@@ -288,6 +168,194 @@ void rentalCar::ReadandStore(std::vector<rentalCar>& classobj)
 	std::cout << "Data read and stored in vector for cars! Now closing file cars.data" << std::endl << std::endl;
 	inFS.close(); // Done with file, so close it
 }
+
+void rentalCar::listAllCars(std::vector<rentalCar>& cars)
+{
+	std::cout << "\n\n" << std::setw(6) << std::left << "ID" << "|" << std::setw(10) << std::left << "  Make" << "|" << std::setw(10) << std::left << "  Model" << std::endl;
+	std::cout << std::setfill('-') << std::setw(26) << "" << std::endl;
+	std::cout << std::setfill(' ');
+
+	for (unsigned int i = 0; i < cars.size(); ++i)
+	{
+		std::cout << std::setw(6) << std::left << cars.at(i).getid() << "|" << std::setw(10) << std::left << cars.at(i).getMake() << "|" << std::setw(10) << std::left << cars.at(i).getModel() << std::endl;
+	}
+	std::cout << std::endl;
+}
+
+
+// searches id against car database and prints results to screen
+void rentalCar::searchId(std::vector<rentalCar>& cars) const
+{
+	{
+		unsigned int searchID;
+
+		std::cout << "you can choose from the following ' MODEL ' :\n";
+		for (unsigned int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different ID available
+			std::cout << std::setw(5) << std::left << cars.at(i).getid() << " | ";
+		std::cout << "\nSearch by ID (first letter is capital for all makes): ";
+		std::cin >> searchID;
+
+		int count = 0;
+		for (unsigned int i = 0; i < cars.size(); ++i)		// lopp to iterate through the database searching for id
+		{
+			if (cars.at(i).getid() == searchID)		//if id found, increases cout and prints out the results
+			{
+				count++;
+				std::cout << "\n\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
+				std::cout << std::setw(6) << std::left << "ID" << "|" << std::setw(10) << std::left << "  Make" << "|" << std::setw(10) << std::left << "  Model" << std::endl;
+				std::cout << std::setfill('-') << std::setw(26) << "" << std::endl;
+				std::cout << std::setfill(' ');
+				std::cout << std::setw(6) << std::left << cars.at(i).getid() << "|" << std::setw(10) << std::left << cars.at(i).getMake() << "|" << std::setw(10) << std::left << cars.at(i).getModel() << std::endl ;
+				break;
+			}			
+		}
+		if (count == 0)		// if count not increased it means id not found and prints respective message to the screen
+		{
+			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
+			std::cout << "Please try again" << std::endl;
+			std::cout << "****************************************************** \n\n" << std::endl;
+			
+		}
+
+	}
+}
+
+
+// searches id against car database and prints results to screen
+void rentalCar::searchMake(std::vector<rentalCar>& cars) const
+{
+	{
+		std::string searchMake;
+
+		std::cout << "you can choose from the following ' Make ' :\n";
+		for (unsigned int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different makes available
+			std::cout << std::setw(5) << std::left << cars.at(i).getMake() << " | ";
+		std::cout << "\nSearch by Make (first letter is capital for all makes): ";
+		std::cin >> searchMake;
+
+		std::cout << std::setw(6) << std::left << "ID" << "|" << std::setw(10) << std::left << "  Make" << "|" << std::setw(10) << std::left << "  Model" << std::endl;
+		std::cout << std::setfill('-') << std::setw(26) << "" << std::endl;
+		std::cout << std::setfill(' ');
+		int count = 0;
+		for (unsigned int i = 0; i < cars.size(); ++i)			// lopp to iterate through the database searching for "MAKE"
+		{
+			if (cars.at(i).getMake() == searchMake)		//if " MAKE " found, increases cout and prints out the results
+			{
+				count++;
+				std::cout << std::setw(6) << std::left << cars.at(i).getid() << "|" << std::setw(10) << std::left << cars.at(i).getMake() << "|" << std::setw(10) << std::left << cars.at(i).getModel() << std::endl ;
+				
+			}
+		}
+		if (count <= 0)			// if count not increased it means "MAKE " not found and prints respective message to the screen
+		{
+			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
+			std::cout << "Please try again" << std::endl;
+			std::cout << "****************************************************** \n\n" << std::endl;
+		}
+		
+	}
+}
+
+void rentalCar::searchModle(std::vector<rentalCar>& cars) const
+{
+	{
+		std::string searchModel;
+
+		std::cout << "you can choose from the following ' MODEL ' :\n";
+		for (unsigned int i(0); i < cars.size(); ++i)									// iterated through the database to pull out all the  different MODEL available
+			std::cout << std::setw(5) << std::left << cars.at(i).getModel() << " | ";
+		std::cout << "\nSearch by MODEL (first letter is capital for all makes): ";
+		std::cin >> searchModel;
+
+		std::cout << std::setw(6) << std::left << "ID" << "|" << std::setw(10) << std::left << "  Make" << "|" << std::setw(10) << std::left << "  Model" << std::endl;
+		std::cout << std::setfill('-') << std::setw(26) << "" << std::endl;
+		std::cout << std::setfill(' ');
+		unsigned int count = 0;
+		for (unsigned int i = 0; i < cars.size(); ++i)			// lopp to iterate through the database searching for "MODEL"
+		{
+			if (cars.at(i).getModel() == searchModel)		//if " MODEL " found, increases cout and prints out the results
+			{
+				count++;
+				std::cout << std::setw(6) << std::left << cars.at(i).getid() << "|" << std::setw(10) << std::left << cars.at(i).getMake() << "|" << std::setw(10) << std::left << cars.at(i).getModel() << std::endl;
+
+			}
+		}
+		if (count <= 0)			// if count not increased it means "MODLE " not found and prints respective message to the screen
+		{
+			std::cout << "\nyou search has resulted in ' " << count << " ' car from our data base" << std::endl;
+			std::cout << "Please try again" << std::endl;
+			std::cout << "****************************************************** \n\n" << std::endl;
+		}
+
+	}
+}
+
+void rentalCar::addCar(std::vector<rentalCar>& cars)
+{
+
+	int newID;
+	std::string newMake;
+	std::string newModel;
+
+	std::cout << "Enter your car ID: ";
+	std::cin >> newID;
+	bool match = false;
+	for (unsigned int i = 0; i < cars.size(); ++i)
+	{
+		if (cars.at(i).getid() == newID)
+		{
+			std::cout << "\n*******Car ID already in use, please use a different ID ********" << std::endl;
+			match = true;
+		}
+	}
+	if (match == false)
+	{
+		std::cout << "Enter your  car Make: ";
+		std::cin >> newMake;
+		std::cout << "Enter your  car Model: ";
+		std::cin >> newModel;
+
+		rentalCar newcar(newID, newMake, newModel);
+		cars.push_back(newcar);
+	}
+
+	
+}
+
+void rentalCar::delCarbyID(std::vector<rentalCar>& cars)
+{
+	std::cout << "To delete a car from database, Please type the car's: ";
+	int delID;
+	std::cin >> delID;
+	bool match = false;
+	for (unsigned int i = 0; i < cars.size(); ++i)
+	{
+		if (cars.at(i).getid() == delID)
+		{
+			cars.erase(cars.begin() + i);
+			std::cout << "\n*******Car ID found and car entry deleted from database ********" << std::endl;
+			match = true;
+		}
+	}
+	if (match == false)
+	{
+		std::cout << "\n car ID not found in the system... Please try again with correct car ID: ";
+
+	}
+}
+
+
+
+
+
+//void rentalCar::apptoCarsData(fstream& out)
+//{
+//	std::cout << id << std::endl;
+//	std::cout << make << std::endl;
+//	std::cout << model << std::endl;
+//}
+
+
 
 //int binarySearch_r(const std::vector<rentalCar>& vi, int target, int low, int high)
 //{
