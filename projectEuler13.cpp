fcd19a959e2306390633adc45e198117c760106f
hamza10vr <ha.mza@hotmail.com>
2021-09-21 17:31:19 -0500

    final verson

--- projectEuler13.cpp
+++ projectEuler13.cpp
@@ -1,3 +1,13 @@
+   
+/*  Project Eculer 13
+    Hamza Tanveer
+    9/21/2021
+    HW 3
+*/
+
+
+
+
 #include<iostream>
 #include<fstream>
 #include<list>
@@ -11,50 +21,48 @@ void addition (std::list<int>& list_1, std::string new_line);   // using 1 linke
 int main()
 {
     std::ifstream inFS;
+    std::list<int> list;
 
 
-    //trying to open file
-    std::cout << "========== opening nums.txt ==========" << std::endl;
-
-    inFS.open("nums.txt");      // opening file
-    if (!inFS.is_open())        // for some reason file doesn't open through out an error and exit
+    inFS.open("nums.txt");      
+    if (!inFS.is_open())        
     {
         std::cout << "Could not open nums.txt" << std::endl;
         return -1; // 1 indicates error
     }
-
     std::cout << "========== filed opened successfully ==========" << std::endl;
 
-    std::list<int> list;
-
-    std::string line;                 // string for reading 50-digit long numbers as string 
+    // reading the first number from file as  string and storing in linked list as single digits
+    std::string line;                 
     inFS >> line;
-
-
-    for (auto i = 0; i < line.length(); ++i)
+    unsigned int i;
+    for ( i = 0; i < line.length(); ++i)    // loop to store 50 digit string into the linked list
     {
-        list.push_front(line.at(i) - '0');
+        list.push_front(line.at(i) - '0');  // converts each character in string to <int> and pushes it to the list
     }
 
-    std::cout << "========== performing calculations ==========" << std::endl;
+    std::cout << "========== performing calculations   ==========\n" << std::endl;
 
-    int a = 1;
-    while (inFS >> line)         // as long as nums.txt has line it will keep reading
+
+    
+    while (inFS >> line)         // as long as nums.txt has line it will keep reading and feeding it to addition function
     {
         addition(list, line);
     }
 
+   
+
+    list.reverse();
 
 
-    std::cout << "===== closing file =====" << std::endl << std::endl;
+    print(list);    // print out the sum
+
+    std::cout << "\n========== closing file    ====================" << std::endl << std::endl;
     inFS.close(); // Done with file, so close it
 
 
 
-    list.reverse();
 
-    
-    print(list);    // print out the sum
 
 
     return 0;
@@ -64,74 +72,98 @@ int main()
 
 
 
-
+//prints out the sum 
 void print (std::list<int>& list)
 {
+    // prints the total sum using range based for-loop
     std::cout << "The sum is: ";
     for (auto number : list)
+    {
         std::cout << number;
+    }
     std::cout << std::endl;
-    int sizee = list.size();
-    std::cout << " size is " << sizee;
+
+    // prints the fist 10 digits, using iterator
+    
+    int n = 0;
+    std::cout << "10 digits =  ";
+    std::list<int>::iterator it; 
+    for (it = list.begin(); it != list.end() && n<10; ++it)
+    {
+        std::cout << *it;
+        n++;
+    }
+
+    
+    // prints the size of the list
+    std::cout << "\nLinked list has: " << list.size() << " nodes\n";
 }
 
+
+// reverses the string before feeding it to the list for addition.
 void reverseStr (std::string& str)
 {
     int n = str.length();
 
-    // Swap character starting from two
-    // corners
+    // Swap character starting from two corners
     for (int i = 0; i < n / 2; i++)
+    {
         std::swap(str[i], str[n - i - 1]);
+    }
 }
 
+
+// add function uses reference to list
+// this allows same list to be modified with new digits instead of creating new list every time
 void addition (std::list<int>& list_1, std::string new_line)
 {
-    std::list<int>::iterator it = list_1.begin();
-    int carry = 0;
-    int digit = -1;
-    unsigned int i = 0;
-    reverseStr(new_line);
+   
+    int carry = 0;      // carry initialy set to 0
+    int digit;         // hold the <int> after being convrted from 'char'
+    unsigned int i = 0; // iterating through the each "char" in 50 "char" long string
+    reverseStr(new_line); // reverses the line read by input file stream
+    std::list<int>::iterator it; // declaration of iterator as it
 
 
-
+    // iterates through each node of list and adding the new number read from file
     for (it = list_1.begin(); it != list_1.end(); ++it)
     {
 
-        if (i < new_line.length())
+        if (i < new_line.length())          
         {
-            digit = new_line.at(i) - '0';
-            *it = digit + *it + carry;
+            digit = new_line.at(i) - '0';   // converts "char" to <int> and store in "digit"
+            *it = *it + digit  + carry;     // update list node value  with = curent digit from list + new digit from file + carry
         }
-        else
+        else                                // no new digits from input file but still digit in list
         {
-            digit = 0;
-            *it = *it + carry;
-        }
-
-
-
-        if (*it >= 10)
-        {
-            carry = 1;
-
-            *it = *it % 10;
-
-        }
-        else
-        {
-            carry = 0;
-
+            *it =  *it + carry;             // add current numer from list + carry 
         }
 
-        i++;
+
+        // if the new number after addition is greater than 10 
+        // store the number in the list as follows
+        if (*it >= 10)          // ex 14
+        {
+            carry = 1;          // carry  = 1
+
+            *it = *it % 10;     // digit stored = 4
+
+        }
+        else                    // if number less than 10 
+        {                       // ex 7 - keep it stored in list
+            carry = 0;          // make carry = 0
+                                
+        }
+
+        i++;                    // increase the interator for reading the next "char" in string
 
 
     }
 
+    // once the calculation is finished and still a carrry. 
     if (carry == 1)
     {
-        list_1.push_back(carry);
+        list_1.push_back(carry);    // add the carry at the end of the list
     }
 
 
