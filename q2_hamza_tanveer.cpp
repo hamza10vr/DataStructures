7289c9bab453991acb4c506b58267602b40b8a3a
hamza10vr <ha.mza@hotmail.com>
2021-10-05 21:14:29 -0500

    amjad version

--- q2_hamza_tanveer.cpp
+++ q2_hamza_tanveer.cpp
@@ -59,31 +59,7 @@ void add_at_head(NodePtr& head, int num)
 }
 
 
-//add a node at the tail of linked list
-//O(n)
 
-void add_at_tail(NodePtr& head, int num)
-{
-	//step 0: when the list is empty, call add at head
-	if (!head)
-	{
-		add_at_head(head, num);
-	}
-
-
-	// step 1: createa a new node
-	NodePtr newNode = new Node(num);
-
-	//step 2 : find the tail node
-	NodePtr temp = head;
-	while (temp && temp->next)
-	{
-		temp = temp->next;
-	}
-
-	//step 3 : link newNode to the list
-	temp->next = newNode;
-}
 
 void addHead(NodePtr& head, int num)
 {
@@ -98,19 +74,7 @@ void addHead(NodePtr& head, int num)
 //search a number in the linked list
 // O (n)
 
-bool llsearch(const NodePtr head, int target)
-{
-	NodePtr temp = head;
-	while (temp != nullptr)  /// same as while (temp)
-	{
-		if (temp->num == target)
-			return true;
 
-		temp = temp->next;
-	}
-
-	return false;
-}
 
 //a search function that searches a given target. Return a node address if found, and return nullptr otherwise
 NodePtr search(const NodePtr& head, NodePtr& target)
@@ -146,47 +110,7 @@ NodePtr findTail(NodePtr& head) {
 
 
 
-//delete target number
-// O(n)
-void del(NodePtr& head, int target)
-{
-	// case 1 : the linked list is empty
-	if (!head)
-		return;
 
-	// Case 2: head node is teh one to be deleted
-	if (head && head->num == target)
-	{
-		NodePtr temp = head;
-		head = head->next;			// works even head->next is nullptr
-		delete temp;
-		temp = nullptr;
-		return;
-
-	}
-
-	// other cases
-	NodePtr prev = nullptr;
-	NodePtr temp = head;
-
-	while (temp && temp->num != target)
-	{
-		prev = temp;
-		temp = temp->next;
-	}
-
-	//case 3: tem is empty , searched linked list but no math
-	if (!temp)
-		return;
-
-	// case 4 : temp is not empty and temp-> is target
-	prev->next = temp->next;
-	delete temp;
-	temp = nullptr;
-
-
-
-}
 
 
 void quiz2(NodePtr& head1, NodePtr& target, NodePtr& head2)
@@ -201,16 +125,16 @@ void quiz2(NodePtr& head1, NodePtr& target, NodePtr& head2)
 		std::cout << "target not found. ";
 		NodePtr tail = findTail(head1);
 
-		if (!tail) 
+		if (!tail)	// if list 1 is empty 
 		{
-			head1 = head2;
+			head1 = head2;	// list 1 = list 2
 			head2 = nullptr;
 			return;
 		}
 
-		NodePtr cur = head2;
-		tail->next = head2;
-		head2 = nullptr;
+		
+		tail->next = head2;		// connecting list 1 tail to list 2
+		head2 = nullptr;		
 
 		
 	}
@@ -225,7 +149,7 @@ void quiz2(NodePtr& head1, NodePtr& target, NodePtr& head2)
 			cur_remove = succ;
 		
 		}
-		target->next = head2;
+		target->next = head2;	
 		head2 = nullptr;
 	}
 
@@ -233,138 +157,78 @@ void quiz2(NodePtr& head1, NodePtr& target, NodePtr& head2)
 }
 
 
+void delList(NodePtr& head) {
+
+	NodePtr cur = head;
+	NodePtr nxt;
+	while (cur) {
+		nxt = cur->next;
+		delete cur;
+		cur = nxt;
+	}
+	head = NULL;
+
+}
+void createTestData(NodePtr& head1, NodePtr& head2) {
+
+	for (auto i : { 50, 40,30,20,10 })
+		add_at_head(head1, i);
+	//findTail(head1)->next = nullptr;
+	for (auto i : { 13,12,11 })
+		add_at_head(head2, i);
+	//findTail(head2)->next = nullptr;
+
+
+}
+
+void testNotFound() {
+	std::cout << "Testing Not found: " << std::endl;
+	NodePtr head1{ nullptr };
+	NodePtr head2{ nullptr };
+	createTestData(head1, head2);
+
+	quiz2(head1, head2, head2);	// test case when when address not found // this is case because h2 is not part of h1 
+	traverse(head1);
+	delList(head1);
+
+}
+
+void testSomeIndex( int test_index) {
+
+	std::cout << "Testing using index: " << test_index << std::endl;
+	NodePtr head1{ nullptr };
+	NodePtr head2{ nullptr };
+	createTestData(head1, head2);
+	NodePtr test_case = head1;
+
+	while (test_case && test_index > 0) {
+		test_case = test_case->next;
+		test_index--;
+	}
+
+	quiz2(head1, test_case, head2);	// test case when when address not found // this is case because h2 is not part of h1 
+
+
+	traverse(head1);
+	delList(head1);
+}
+
 int main()
 {
 
 	using std::cout;
 	using std::endl;
 
-	//NodePtr head{ NULL };
-	NodePtr head1 {nullptr};
+	NodePtr head1{ nullptr };
 	NodePtr head2{ nullptr };
 
-	for (auto i : { 40,30,20,10 })
-		add_at_head(head1, i);
 
-	for (auto i : { 13,12,11 })
-		add_at_head(head2, i);
-
-	traverse(head1);
-	traverse(head2);
-
-	NodePtr taill = findTail(head1);
-	NodePtr test_case = head1; // 10 , 20, 30, 40 //  10
-	int test_index = 2;	
-
-	while (test_case && test_index>0) {	// 10 && 1 > 0 
-		test_case = test_case->next;		//20
-		test_index--;					// 0
+	testNotFound(); // 
+	for (int i = 0; i < 5; i++) {
+		testSomeIndex(i);
 	}
-	quiz2(head1, test_case, head2);
-
-	traverse(head1);
-
-
 	
-	
-	//auto target_not_found = search(head1, 50);
-	//quiz2(head1, target_not_found, head2);
-
-	std::cout << "\nprinting head1 : ";
-	traverse(head1);
-
-	std::cout << "\nprinting head 2 which shouldn't exist : ";
-	traverse(head2);
-
-
-
-	std::cout << "\n testing if target found ******* \n : ";
-
-	NodePtr head3{ nullptr };
-	NodePtr head4{ nullptr };
-
-	for (auto i : { 70,60,50,40,30,20,10 })
-		add_at_head(head3, i);
-
-	for (auto i : { 13,12,11 })
-		add_at_head(head4, i);
-
-	traverse(head3);
-	traverse(head4);
-
-
-
-	//auto target_address = search(head3, 20);
-	//quiz2(head3, target_address, head4);
-
-	std::cout << "\nprinting head3 : ";
-	traverse(head3);
-
-	std::cout << "\nprinting head 4 which shouldn't exist : ";
-	traverse(head4);
-
-
-
-
-
-
-	//find the target
-	// assign temp pointer to target
-	// delete nodes in list 1 until null ptr
-			// temp 1 hold next point and temp 2 holds current pointer 
-	// merge linked list 2 to 1
-
-
-
-
-
-
-	
-	
-
-	//head = new Node{ 10 };
-
-	//// how can i refer to the next
-	//// assign the newly created node's addreess to the next fille dinside the head/first node
-	//head->next = new Node{ 20 };
-	////(*head).next = new Node{ 20 };
-
-	//head->next->next = new Node{ 30 };
-
-	//head->next->next->next = new Node{ 40 };
-
-	//head->next->next->next->next = new Node{ 750 };
-
-	//del(head, 23);
-
-	//for (auto i : { 50,30,20,10 })
-	//	add_at_head(head, i);
-
-	//for (auto i : { 101, 202, 303, 404 })
-	//	add_at_tail(head, i);
-
-	//traverse(head);
-
-	//cout << endl << std::boolalpha << llsearch(head, 202) << endl;
-	//cout << std::boolalpha << llsearch(head, 22) << endl;
-
-	//std::cout << std::endl;
-	//del(head, 10);
-	//traverse(head);
-
-	//std::cout << std::endl;
-	//del(head, 5050);
-	//traverse(head);
-
-	//std::cout << std::endl;
-	//del(head, 101);
-	//traverse(head);
-
-	//std::cout << std::endl;
-	//del(head, 404);
-	//traverse(head);
 
 	return 0;
-
 
 }
